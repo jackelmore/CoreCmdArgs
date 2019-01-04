07c4e355818d2a447a3bbb562ddf6f52fce4944b
Jack Elmore <jackelmore@users.noreply.github.com>
2019-01-03 20:47:49 -0800

    Add project files.

---
+++ CoreCmdArgs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using PowerArgs;
+
+namespace CoreCmdArgs
+{
+    public class MyArgs
+    {
+        [ArgRequired(PromptIfMissing = false)]
+        public string StringArg { get; set; }
+        [ArgRange(1, 10)]
+        public int IntArg { get; set; }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                var parsed = Args.Parse<MyArgs>(args);
+                Console.WriteLine("You entered string '{0}' and int '{1}'", parsed.StringArg, parsed.IntArg);
+            }
+            catch (ArgException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ArgUsage.GenerateUsageFromTemplate<MyArgs>());
+            }
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
